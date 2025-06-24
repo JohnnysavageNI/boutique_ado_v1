56259e5addd6ec7267ac2ad36fb02c9bf13e6419
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-06-24 20:05:13 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RdbgFI1blLJ41KyInEzQqf2KdsNoZ4XhyvBpdSIjf8OfIBK7SPs0uwpJBIMXkJD7NBAsPchuUxVptJgdVnj8zkF00FUxbykd3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
